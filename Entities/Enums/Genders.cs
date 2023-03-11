df15d6e2abd737601e034eec8d456a9bc5ff455a
Mert Can Akdoğan <mertcakdogan@gmail.com>
2023-03-11 14:12:20 +0300

    Entities Enums da Genders oluşturuldu.

---
+++ Entities/Enums/Genders.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.Enums
+{
+    public enum Genders
+    {
+        /// <summary>
+        /// Kadın
+        /// </summary>
+        Famele=0,
+        /// <summary>
+        /// Erkek
+        /// </summary>
+        Male=1
+    }
+}
