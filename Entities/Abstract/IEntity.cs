8994c93803196f26f1ad8adfddf7d29f4e07cd7f
Mert Can Akdoğan <mertcakdogan@gmail.com>
2023-03-11 14:09:15 +0300

    Entities Abstract da IEntity oluşturuldu.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Abstract
+{
+    //<summary>
+    //Veritabanındaki karşılık gelen tablolarda olacak
+    //<summary>
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
